fe5a33d97ac9e26f549fa00d728308d5e6ddc5fa
daniel <g.thirteen333@gmail.com>
2019-11-08 20:07:47 -0500

    validate full url method complete

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -3,13 +3,24 @@ class ShortUrlsController < ApplicationController
   # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
 
+  def new
+    @short_url = ShortUrl.new
+  end
+
   def index
+    @short_urls = ShortUrl.all
   end
 
   def create
   end
 
   def show
+    @short_url = ShortUrl.find(params[:id])
+  end
+
+  private
+  def short_url_params
+    params.require(:short_url).permit(:full_url)
   end
 
 end
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,7 +2,7 @@ class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
-  validate :validate_full_url
+  validates :validate_full_url
 
   def short_code
   end
@@ -10,9 +10,11 @@ class ShortUrl < ApplicationRecord
   def update_title!
   end
 
-  private
+  
 
   def validate_full_url
+    return self.include?("http:") || self.include?("https:")
   end
+     
 
 end
